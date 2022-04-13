e09a05fdd2c83519106e0bb43a762ac0497fe639
Kurisu1129 <469723183@qq.com>
2022-04-14 00:09:46 +0800

    添加消息组件

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+  <el-scrollbar height="400px">
+    <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
+  </el-scrollbar>
+      </el-main>
+    </el-container>
+</template>
+
+<script>
+import avator1 from "../assets/avator1.png"
+import avator2 from "../assets/avator2.png"
+export default {
+  name: 'IndexPage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      sender: avator1,
+      receiver: avator2
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
+</style>
+
---
+++ src/components/message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+        <el-row>
+    <el-col :span="12"><div class="grid-content bg-purple" /></el-col>
+    <el-col :span="12"><div class="grid-content bg-purple-light" /></el-col>
+  </el-row>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
